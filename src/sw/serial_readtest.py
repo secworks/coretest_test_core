6b473d2d9848036ce3e8917ecc7ad3d7b1f2048d
Joachim Strömbergson <joachim@secworks.se>
2014-03-12 15:33:30 +0100

    Adding threaded based response parser.

---
+++ src/sw/serial_readtest.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#=======================================================================
+#
+# serial_readtest.py
+# -----------------
+# Program that tries to create a thread/process that observes the
+# serial read interface and reacts to data appearing. In the
+# main program we send commands to trigger reaction from coretest.
+#
+# Note: This proram requires the PySerial module.
+# http://pyserial.sourceforge.net/
+#
+# 
+# Author: Joachim Strömbergson
+# Copyright (c) 2014  Secworks Sweden AB
+# 
+# Redistribution and use in source and binary forms, with or 
+# without modification, are permitted provided that the following 
+# conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer. 
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#=======================================================================
+ 
+#-------------------------------------------------------------------
+# Python module imports.
+#-------------------------------------------------------------------
+import sys
+import serial
+import os
+import time
+import threading
+
+ 
+#-------------------------------------------------------------------
+# Defines.
+#-------------------------------------------------------------------
+VERBOSE = False
+done = False
+
+
+#-------------------------------------------------------------------
+# print_response()
+#
+# Parses a received buffer and prints the response.
+#-------------------------------------------------------------------
+def print_response(buffer):
+    if VERBOSE:
+        print "Length of response: %d" % len(buffer)
+        if buffer[0] == '\xaa':
+            print "Response contains correct Start of Response (SOR)"
+        if buffer[-1] == '\x55':
+            print "Response contains correct End of Response (EOR)"
+
+    response_code = ord(buffer[1])
+
+    if response_code == 0xfe:
+        print "UNKNOWN response code received."
+
+    elif response_code == 0xfd:
+        print "ERROR response code received."
+
+    elif response_code == 0x7f:
+        read_addr = ord(buffer[2]) * 256 + ord(buffer[3])
+        read_data = (ord(buffer[4]) * 16777216) + (ord(buffer[5]) * 65536) +\
+                    (ord(buffer[6]) * 256) + ord(buffer[7])
+        print "READ_OK. address 0x%02x = 0x%04x." % (read_addr, read_data)
+
+    elif response_code == 0x7e:
+        read_addr = ord(buffer[2]) * 256 + ord(buffer[3])
+        print "WRITE_OK. address 0x%02x." % (read_addr)
+
+    elif response_code == 0x7d:
+        print "RESET_OK."
+
+    else:
+        print "Response 0x%02x is unknown." % response_code
+        print buffer
+        
+
+#-------------------------------------------------------------------
+# read_serial_thread()
+#
+# Function used in a thread to read from the serial port and
+# collect response from coretest.
+#-------------------------------------------------------------------
+def read_serial_thread(serialport):
+    print "Serial port response thread started."
+    print "Waiting for response..."
+    
+    count = 0
+    buffer = []
+    while not done:
+        response = serialport.read()
+        buffer.append(response)
+        if response == '\x55':
+            print_response(buffer)
+            buffer = []
+
+    
+#-------------------------------------------------------------------
+# main()
+#
+# Parse arguments.
+#-------------------------------------------------------------------
+def main():
+    # Open device
+    ser = serial.Serial()
+    ser.port='/dev/cu.usbserial-A801SA6T'
+    ser.baudrate=9600
+    ser.bytesize=8
+    ser.parity='N'
+    ser.stopbits=1
+    ser.timeout=1
+    ser.writeTimeout=0
+    ser.open()
+    done = False
+
+    # Try to create a thread
+    try:
+        my_thread = threading.Thread(target=read_serial_thread, args=(ser))
+    except:
+        print "Error: Can't start thread."
+        sys.exit()
+        
+    print type(my_thread)
+    print dir(my_thread)
+    my_thread.start()
+
+    # Send the command sequence.
+#    for i in range (16):
+#        print "Sending write command: address 0x0120 = 0x%02x." % i
+#        my_cmd = ['\x55', '\x11', '\x01', '\x20', '\x00', '\x00', '\x00']
+#        my_cmd.append(chr(i))
+#        my_cmd.append('\xaa')
+#        for tx_byte in my_cmd:
+#            ser.write(tx_byte)
+#            time.sleep(0.05)
+
+    done = True
+    my_thread.exit()
+    ser.close()
+    
+    # Exit nicely.
+    if VERBOSE:
+        print "Done. Closing device."
+
+
+#-------------------------------------------------------------------
+# __name__
+# Python thingy which allows the file to be run standalone as
+# well as parsed from within a Python interpreter.
+#-------------------------------------------------------------------
+if __name__=="__main__": 
+    # Run the main function.
+    sys.exit(main())
+
+#=======================================================================
+# EOF serial_write_sequence.py
+#=======================================================================
